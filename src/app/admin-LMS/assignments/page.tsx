d4efba73dd3098142e9b9984cc937cf3b887dd7c
Ramika Perera <ramikaperera4@gmail.com>
2026-08-15 00:56:37 +0530

    error

--- src/app/admin-LMS/assignments/page.tsx
+++ src/app/admin-LMS/assignments/page.tsx
@@ -154,7 +154,7 @@ export default function AssignmentsAndQuizzes() {
   const [uploadedFile, setUploadedFile] = useState<File | null>(null);
 
   // Click outside listener to close dropdowns
-  const dropdownRef = useRef<HTMLDivElement>(null);
+  const dropdownRef = useRef<HTMLTableDataCellElement>(null);
   useEffect(() => {
     function handleClickOutside(event: MouseEvent) {
       if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
